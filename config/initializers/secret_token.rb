179a19aceee3f3c95b6e227bb45a1f0ce9f8c79a
William M. Delaney <willdelaney@gmail.com>
2013-07-25 23:23:25 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditCup::Application.config.secret_token = 'be22321062cc9963e06ac14dbacb1c37858f85fb9406c4f427735078b72be556a0faec07e053488fd30df8ae8343746510459fe4f85aa36ac7faaa3f0a80ddcc'
